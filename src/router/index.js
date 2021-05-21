9a15c38b366a4f9dd3970ea26e2f3c6fecfd3975
Knut Sandin <knutmonason@gmail.com>
2021-05-21 09:46:56 +0200

    start working

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import IndexPage from '@/views/Index/Index.vue'
+import StartPage from '@/views/start.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     { path: '/', name: 'index', component: IndexPage },
+    { path: '/start', name: 'start', component: StartPage },
     { path: '*', redirect: { name: 'index' } }
+
   ]
 })
---
+++ src/views/start.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Dashboard</h1>
+</template>
+
+<style scoped>
+  h1 {
+    font-size: 30px;
+    font-family: monospace;
+  }
+</style>
+
+<script>
+  
+</script>
